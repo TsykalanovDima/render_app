9a882caf88d1984c6df4cc0e895aab03b00730cb
Lumos <tsykalanovdima@gmail.com>
2024-06-22 19:47:12 +0100

    commit

--- app_api.py
+++ app_api.py
@@ -2,18 +2,21 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def hello_world():
-    return '`Hello World!'
+    return '`Hello LOX!'
+
 
 @app.route('/greet', methods=['POST'])
 def greet():
     data = request.get_json()
     if 'name' in data:
         name = data['name']
-        return jsonify(message=f'Hello, {name}!')
+        return jsonify(message=f'You are LOX, {name}?')
     else:
         return jsonify(error='noName'), 400
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=81)
--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-url = 'https://render-app-6k9t.onrender.com/greet'
-data = {'name': 'Dima'}
+url = 'http://127.0.0.1:81/greet'
+data = {'name': 'Vitala'}
 headers = {'Content-Type': 'application/json'}
 
 response = requests.post(url, headers=headers, data=json.dumps(data))
